9be0d02911d2b2117d46d048747ff67771abb222
indu <70087467+lazyindu@users.noreply.github.com>
2023-01-27 02:42:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -22,7 +22,7 @@ from database.filters_mdb import (
     get_filters,
 )
 import os
-log_channel = int(os.environ.get('LOG_CHANNEL','-1001820894005'))
+req_channel = int(os.environ.get('REQ_CHANNEL','-1001855682845'))
 
 import logging
 
@@ -629,7 +629,7 @@ async def auto_filter(client, msg, spoll=False):
             search = message.text
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
             if not files:
-                await client.send_message(log_channel,f"🦋 #REQUESTED_CONTENT 🦋,\n\n📝**Content Name** :`{search}`\n**Requested By**: {message.from_user.first_name}\n **USER ID**:{message.from_user.id}\n\n🦋",
+                await client.send_message(req_channel,f"-🦋 #REQUESTED_CONTENT 🦋-,\n\n📝**Content Name** :`{search}`\n**Requested By**: {message.from_user.first_name}\n **USER ID**:{message.from_user.id}\n\n🗃️",
                                                                                                        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔺 Mark as Done 🔺", callback_data="close_data")]]))
      
                 if settings["spell_check"]:
