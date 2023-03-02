3095a65d89414fbc00308536d3b0e1c17f1aea6a
vkthouseef <122280249+vkthouseef@users.noreply.github.com>
2023-03-03 00:11:51 +0530

    Fixed All ERRORS....

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,4 +1,6 @@
-# @LazyDeveloper
+    # Credit @LazyDeveloper.
+    # Please Don't remove credit.
+    # Thank you LazyDeveloper for helping us in this Journey
 import asyncio
 import re
 import ast
@@ -8,21 +10,26 @@ from Script import script
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
-from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, IMDB, \
+from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, IMDB,REQ_CHANNEL, \
     SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ForceReply
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
 from database.users_chats_db import db
 from database.ia_filterdb import Media, get_file_details, get_search_results
+from database.lazy_utils import progress_for_pyrogram, convert, humanbytes
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+import os 
+import humanize
+from PIL import Image
+import time
 from database.filters_mdb import (
     del_all,
     find_filter,
     get_filters,
 )
-
-import os
 req_channel = int(os.environ.get("REQ_CHANNEL", "-1001714473720"))
 
 import logging
